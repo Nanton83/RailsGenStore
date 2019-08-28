67e0d45bb4128b9d7c5a113d89235f3693c6a10c
nanton83 <nicholas.romans@gmail.com>
2019-08-28 15:38:37 -0400

    session controller added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,30 @@ class UsersController < ApplicationController
         @user = User.new
     end 
 
+    def create
+        @user = User.new(user_params)
+      #   binding.pry
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        if !current_user.distributer
+            if current_user != @user
+                redirect_to root_path
+            end
+        end
+    end
+  
+    private
+    
+    def user_params
+        params.require(:user).permit(:user_name, :password, :cubits, :distributer)
+    end
+
 end
