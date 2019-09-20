bb9bd38837f1b72aab48b52d8188b65cdb43a072
nanton83 <nicholas.romans@gmail.com>
2019-09-20 10:46:21 -0400

    fixing git

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
             @items = Store.find(params[:store_id]).items
         else
             @items = Item.all
+            # need to update
         end
     end
 
