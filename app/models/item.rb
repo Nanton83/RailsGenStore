247584a8e19acd8ab42f294adc377c6bab203ddc
Nicholas Romans <Nicholas.Romans@gmail.com>
2019-09-24 15:04:09 -0400

    working styles through views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord   
     belongs_to :distributor
     belongs_to :store
-
+    validates :name, :presence => true
+    validates :brand_name, :presence => true
+    validates :sku, :presence => true
+    validates :price, :presence => true
     scope :branded, -> (brand_name) {where("brand_name LIKE ?", brand_name)}
 
     # def branded(brand_name)
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,6 @@
 class Store < ApplicationRecord
     has_many :items
     has_many :distributors, through: :items
-    
+    validates :name, :presence => true
+    validates :location, :presence => true
 end
